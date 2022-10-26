43d70e9b9b604213a787e3e398ead46253d852e5
Ashutoshrx <asutosh007satpathy@gmail.com>
2022-10-26 22:06:25 +0530

    added feedback stats component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 import { useState } from 'react';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
   //Use of UseState which is a react hook
   const [feedBackData, setFeedBackData] = useState(FeedbackData);
   const deleteFeedbackData = (id) => {
-    console.log(`The feedback data has been deleted with id ${id}`);
+    // console.log(`The feedback data has been deleted with id ${id}`);
     setFeedBackData(feedBackData.filter((fd) => fd.id !== id));
   };
 
@@ -15,6 +16,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedBackData} />
         <FeedbackList
           feedback={feedBackData}
           handleDelete={deleteFeedbackData}
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function FeedbackStats({ feedback }) {
+    let feedbackIds = feedback.map(fb => fb.rating);
+    let sumOfIds = feedbackIds.reduce((total, id) =>
+        total + id, 0
+    );
+    let avgOfIds = sumOfIds / feedback.length; // average rating
+    if (feedback.length === 0) {
+        return;
+    } else {
+        return (
+            <div className='feedback-stats'>
+                <h4>{feedback.length} Reviews</h4>
+                <h4>Average Ratings : {avgOfIds}</h4>
+            </div>
+        );
+    }
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
+    )
+};
+export default FeedbackStats;
